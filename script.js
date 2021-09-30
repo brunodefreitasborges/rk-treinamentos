347c642c97a69489f77cb1d97fdc9c2d206dcc43
Bruno de Freitas <brunofreitas.ads@gmail.com>
2021-09-30 09:26:12 -0300

    Added Overlay

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function expandirCursos() {
 
     if (document.getElementsByClassName("card-curso").length < 9) {
     
-    document.getElementById("cards-cursos").innerHTML += '<div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p></div><div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p></div><div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p></div>';
+    document.getElementById("cards-cursos").innerHTML += '<div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p><button>Mais Informações</button></div><div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p><button>Mais Informações</button></div><div class="card-curso"><h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src="assets/nr11.jpg" alt="Imagem de uma empilhadeira"><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p><button>Mais Informações</button></div>';
 
     document.getElementById("expand").scrollIntoView(false);
 
@@ -42,3 +42,51 @@ function expandirCursos() {
     }
 
 }
+
+function expandirCard(card) {
+
+    
+
+    
+
+    
+    
+}
+
+
+/* Open when someone clicks on the span element */
+function openNav(card) {
+    document.getElementById("myNav").style.height = "100%";
+
+    if(card.id == "curso1-btn") {
+        
+        curso = document.getElementById("curso1");
+        console.log(card.id)
+        
+    }
+
+    else if(card.id == "curso2-btn") {
+        curso = document.getElementById("curso2");
+        console.log(card.id)
+        
+    }
+
+    else if(card.id == "curso3-btn") {
+        curso = document.getElementById("curso3");
+        console.log(card.id)
+        
+    }
+
+    content = document.getElementById("overlay-content");
+
+    content.innerHTML = "<h3>TREINAMENTO OPERADOR DE MÁQUINAS (NR11)</h3><img src='assets/nr11.jpg' alt='Imagem de uma empilhadeira'><p>Todos os equipamentos de força motriz própria empilhadeira, paleteiras, empilhadeira elétrica, ponte rolantes, talhas, monovias, guinchos, munck. devem ser manuseados por empregados habilitados pela empresa, sendo que a habilitação neste caso consiste em treinamento específico por empresa especializada em tal segmento. Esses cursos têm caráter obrigatório conforme NR11 do Ministério do Trabalho.</p>"
+
+
+
+
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
